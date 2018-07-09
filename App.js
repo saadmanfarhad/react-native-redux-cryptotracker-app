c46d618d53d6a013d8a5a514c9a2f32e0cdacf52
saadmanfarhad <saadmanheavy@gmail.com>
2018-07-09 15:44:30 +0600

    Added CryptoContainer in App.js

--- App.js
+++ App.js
@@ -3,14 +3,15 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 
 import Store from './src/Store';
-import { Header } from './src/components';
+import { Header, CryptoContainer } from './src/components';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store = {Store}>
         <View>
           <Header/>
+          <CryptoContainer/>
         </View>
       </Provider>
     );
